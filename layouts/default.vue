3dac23fe719f48a3c113c93a625f4934328f96ee
amirpeace1080 <amirpeace1080@gmail.com>
2021-02-01 22:46:49 +0330

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Navbar />
+    <v-app >
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components:{
+    Navbar
+  },
+  data() {
+    return {
+      //
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="subtitle">Home Page</h1>
+    <span class="display-4">hell</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
